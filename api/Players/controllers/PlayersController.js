e17cace5028fe45aded0bbdfe23b1d2f78ac0ccb
Brunnosama <apbrunno@gmail.com>
2022-10-29 21:10:30 -0300

    chore: tests pratice

--- api/Players/controllers/PlayersController.js
+++ api/Players/controllers/PlayersController.js
@@ -1,12 +1,68 @@
-class PlayersController {
-    static async getAll(req, res) {
-        return res.send("ClassController");
-    }
+// class PlayersController {
+//     static async getAll(req, res) {
+//         return res.send("ClassController");
+//     }
 
-    static sum(a, b) {
-        let sum = a + b;
-        return sum;
+//     static sum(a, b) {
+//         let sum = a + b;
+//         return sum;
+//     }
+// }
+
+// module.exports = PlayersController;
+
+const sum = (a, b) => {
+    let sum = a + b;
+    return sum;
+};
+
+const name = (name) => {
+    let fullName = name + " Pessoa"
+    return fullName;
+};
+
+const verifyItems = (arr) => {
+    let newArr = arr.map((item) => item);
+    for (let index = 0; index <= newArr.length; index++) {
+        if (!newArr[index]) {
+            return false
+        }
+        return newArr[index];
     }
+    return newArr;
+};
+
+const page1 = (req, res) => {
+    res.send("Page1");
+};
+
+const testReq = (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.send("Invalid Id")
+    }
+    if (id === 1) {
+        return res.send(`The Id is equal to: ${id}`)
+    } else {
+        return res.send(`The id it's not equal to: ${1}`)
+    }
+};
+
+const testQueryString = (req, res) => {
+    const { num1, num2 } = req.query;
+
+    if (num1 && num2) {
+        res.status(200);
+        return res.send("The params are correct");
+    }
+    return res.send("The params are not true");
 }
 
-module.exports = PlayersController;
+module.exports = {
+    sum,
+    name,
+    verifyItems,
+    page1,
+    testReq,
+    testQueryString
+};
--- api/Players/tests/playersController.test.js
+++ api/Players/tests/playersController.test.js
@@ -1,9 +1,21 @@
-const { expect } = require("chai");
+const expect = require("chai").expect;
 const { it } = require("mocha");
+const sinon = require("sinon");
 const playersController = require("../controllers/PlayersController");
 
-describe("testing the controller class with it's respective methods", () => {
-    
+// describe("testing the controller class with it's respective methods", () => {
+
+//     it("testing the sum method", () => {
+//         let a = 14;
+//         let b = 36;
+//         let result = 50;
+//         expect(playersController.sum(a, b)).to.equal(result);
+//     });
+
+// });
+
+describe("testing the simple function", () => {
+
     it("testing the sum method", () => {
         let a = 14;
         let b = 36;
@@ -11,4 +23,102 @@ describe("testing the controller class with it's respective methods", () => {
         expect(playersController.sum(a, b)).to.equal(result);
     });
 
+    it("testing name function", () => {
+        expect(playersController.name("Brunno")).to.equal("Brunno Pessoa");
+    });
+
+    it("testing array function", () => {
+        let arr = [10, 30, 40];
+        expect(playersController.verifyItems(arr)).to.equal(10);
+    });
+
+    it("testing the page1 method", () => {
+        let res = {
+            send: () => { }
+        }
+        let mock = sinon.mock(res)
+        mock.expects("send").once().withArgs("Page1")
+        playersController.page1({}, res)
+    });
+});
+
+describe("Testing the reqs and res from testReq method", () => {
+    it("verifying if the req is a valid id", () => {
+        let req = {
+            params: {
+                id: ""
+            }
+        }
+        let res = {
+            send: () => { }
+        }
+        let mock = sinon.mock(res)
+        mock.expects("send").once().withArgs("Invalid Id")
+        playersController.testReq(req, res)
+    });
+
+    it("verifying if the req id is equal one", () => {
+        let req = {
+            params: {
+                id: 1
+            }
+        }
+        let res = {
+            send: () => { }
+        }
+        let mock = sinon.mock(res)
+        mock.expects("send").once().withArgs("The Id is equal to: 1");
+        playersController.testReq(req, res);
+    });
+
+    it("verifying if the req id is different than one", () => {
+        let req = {
+            params: {
+                id: 2
+            }
+        }
+        let res = {
+            send: () => { }
+        }
+        let mock = sinon.mock(res)
+        mock.expects("send").once().withArgs("The id it's not equal to: 1");
+        playersController.testReq(req, res);
+    });
+});
+
+describe("Testing the testQuery method", () => {
+    it("verifying it the queries reqs are valid", () => {
+
+        let req = {
+            query: {}
+        };
+
+        let res = {
+            send: () => { }
+        };
+
+        let mock = sinon.mock(res);
+        mock.expects("send").once().withArgs("The params are not true");
+        playersController.testQueryString(req, res);
+    });
+
+    it("verifying if the queries are true", () => {
+
+        let req = {
+            query: {
+                num1: "Brunno",
+                num2: "Pessoa"
+            }
+        };
+
+        let res = {
+            send: () => {},
+            status: () => {}
+        };
+
+        let mock = sinon.mock(res);
+        mock.expects("status").once().withArgs(200);
+        mock.expects("send").once().withArgs("The params are correct");
+        playersController.testQueryString(req, res);
+    });
 });
